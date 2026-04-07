f2bae0b97e86642e5cec434b8c2455821aa3e1b4
Ketcho7 <camdenkackley@icloud.com>
2026-04-07 11:52:09 -0400

    Remove extra spaces in getUserMedia call

--- NatureID/script.js
+++ NatureID/script.js
@@ -3,7 +3,7 @@ const video = document.getElementById('webcam');
 async function startCamera() {
     try {
         // Request video only (audio: false)
-        const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" }, audio: false });
+        const stream = await navigator.mediaDevices.getUserMedia({video: { facingMode: "environment" }, audio: false});
         
         // Set the source of the video element to the stream
         video.srcObject = stream;
